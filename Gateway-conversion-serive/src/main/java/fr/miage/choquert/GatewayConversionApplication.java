f3f0e0fb6eee96dd422fb70ea457ccae1f8855ea
vincent <vincent.choquert4@etu-univ-lorraine.fr>
2022-01-10 13:42:39 +0100

    ft (gw-conversion) : setup gateway conversion

---
+++ Gateway-conversion-serive/src/main/java/fr/miage/choquert/GatewayConversionApplication.java
@@ -0,0 +1,11 @@
+package fr.miage.choquert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayConversionApplication {
+
+    public static void main(String[] args) { SpringApplication.run(GatewayConversionApplication.class, args); }
+
+}
